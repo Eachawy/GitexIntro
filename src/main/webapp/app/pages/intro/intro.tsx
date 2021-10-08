5622a3b9eeef8db425b8d90b1846cb9868258e9c
Eachawy <eachawy@Eachawys-MacBook-Pro.local>
2021-10-08 15:38:23 +0400

    init

---
+++ src/main/webapp/app/pages/intro/intro.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { translate } from "react-jhipster";
+
+const IntroComponent = props => {
+    return (
+        <div>
+            {translate('pages.intro')}
+        </div>
+    );
+}
+
+export default IntroComponent;
