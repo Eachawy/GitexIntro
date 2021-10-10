3fcc3f8197501d5022f1679f9e62cc44dc30e5d7
Eachawy <Eachawy@gmail.com>
2021-10-10 17:34:47 +0400

    Add configuration for firebase with function take image

--- src/main/webapp/app/pages/takePhoto-page/takePhotoPage.tsx
+++ src/main/webapp/app/pages/takePhoto-page/takePhotoPage.tsx
@@ -7,6 +7,15 @@ const TakePhotoComponent = props => {
 
     const [avatar, setAvatar] = React.useState(null);
 
+    const onChangeFile = (e: any) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader: any = new FileReader();
+            reader.readAsBinaryString(file);
+            reader.onload = () => { setAvatar(`data:image/jpeg;base64,${btoa(reader.result)}`) };
+        }
+    }
+
     return (
         <>
             <Link to="/nationality" className="backAction" />
@@ -15,22 +24,23 @@ const TakePhotoComponent = props => {
                 {!avatar &&
                     <div className="launchCamera">
                         <span>Launch camera</span>
+                        <input type="file" accept="image/*" onChange={(e: any) => onChangeFile(e)} capture="camera"></input>
                     </div>
                 }
                 {avatar &&
-                    <>
-                        <div className={`large avatar ${avatar}`} />
+                    <div className="imgProf">
+                        <img src={avatar} />
                         <div className="edit" onClick={() => setAvatar(null)}>Edit</div>
-                    </>
+                    </div>
                 }
                 <p>or choose an avatar</p>
                 <ul>
-                    <li className="avatar _1" onClick={() => setAvatar('_1')} />
-                    <li className="avatar _2" onClick={() => setAvatar('_2')} />
-                    <li className="avatar _3" onClick={() => setAvatar('_3')} />
-                    <li className="avatar _4" onClick={() => setAvatar('_4')} />
-                    <li className="avatar _5" onClick={() => setAvatar('_5')} />
-                    <li className="avatar _6" onClick={() => setAvatar('_6')} />
+                    <li className="avatar _1" onClick={() => setAvatar('../../../content/images/avatar/1.svg')} />
+                    <li className="avatar _2" onClick={() => setAvatar('../../../content/images/avatar/2.svg')} />
+                    <li className="avatar _3" onClick={() => setAvatar('../../../content/images/avatar/3.svg')} />
+                    <li className="avatar _4" onClick={() => setAvatar('../../../content/images/avatar/4.svg')} />
+                    <li className="avatar _5" onClick={() => setAvatar('../../../content/images/avatar/5.svg')} />
+                    <li className="avatar _6" onClick={() => setAvatar('../../../content/images/avatar/6.svg')} />
                 </ul>
                 <Link to="/uploadLicense" className="btnAction">Next</Link>
             </div>
